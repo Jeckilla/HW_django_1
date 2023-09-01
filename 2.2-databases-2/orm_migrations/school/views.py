15c4e386fe4966e119cfc0fbb2a9c9b941eab751
Evgeniya Biryulina <jeckilla@gmail.com>
2023-09-01 20:25:12 +0300

    corrected tamplate students_list

--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -6,8 +6,7 @@ from school.models import Student, Teacher
 
 def students_list(request):
     template = 'school/students_list.html'
-    ordering = 'st_group'
-    students = Student.objects.all().order_by(ordering)
+    students = Student.objects.all().prefetch_related('teachers').order_by('st_group')
     print(students)
 
     return render(request, template, context = {'object_list': students})
